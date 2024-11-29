51204a7fc942b4e2204c2c5f47054f58e1227528
wisdom oladipupo <wisbel71@gmail.com>
2024-11-29 21:47:23 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("registrationForm").addEventListener("submit", (event) => {
+event.preventDefault()
+
+const name = document.getElementById("name").value.trim()
+const email = document .getElementById("email").value.trim()
+const age = document.getElementById("age").value
+const gender = document.getElementById("gender").value
+const terms = document.getElementById("terms").value
+
+let errors = []
+    
+})
