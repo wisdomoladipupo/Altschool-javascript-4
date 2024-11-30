41046d9472998fec52033b9debecb8e108e3dfa9
wisdom oladipupo <wisbel71@gmail.com>
2024-11-30 13:01:42 +0100

    second commit

--- index.js
+++ index.js
@@ -8,5 +8,30 @@ const gender = document.getElementById("gender").value
 const terms = document.getElementById("terms").value
 
 let errors = []
-    
+
+if (name.length < 3){
+    errors.push("name must be atleast three character long")
+}
+
+if (!/\S+@\S+\.\S+/.test(email)){
+    errors.push("invalid email format")
+}
+
+if(age < 18 || age > 99){
+    errors.push("age must be within 18 and 99")
+}
+
+if(!gender){
+    errors.push("you must select a gender")
+}
+ if(!terms){
+    errors.push("you must agree to the terms and condition")
+ }   
+
+ if (errors.length > 0){
+    alert.join("\n")
+ }else{
+    alert("form submitted submitted succesfully")
+    this.reset()
+ }
 })
